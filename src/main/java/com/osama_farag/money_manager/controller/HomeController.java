6f9b6a7aaf931df200f89e8ae5985827875a1db6
Osama Farag <ofarag115@gmail.com>
2026-03-21 09:01:20 -0400

    imported restconroller, requestmapping, and getmapping for Homecontroller

--- src/main/java/com/osama_farag/money_manager/controller/HomeController.java
+++ src/main/java/com/osama_farag/money_manager/controller/HomeController.java
@@ -1,7 +1,14 @@
 package com.osama_farag.money_manager.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/status", "/health"})
 public class HomeController {
 
+    @GetMapping
     public String healthCheck() {
         return "Application is running successfully!";
     }
