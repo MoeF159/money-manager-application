a37a6d544073f0e18cb4808f668cb9e4b572506b
Osama Farag <ofarag115@gmail.com>
2026-03-21 08:37:28 -0400

    created the HomeController class with a healthCheck method

---
+++ src/main/java/com/osama_farag/money_manager/controller/HomeController.java
@@ -0,0 +1,8 @@
+package com.osama_farag.money_manager.controller;
+
+public class HomeController {
+
+    public String healthCheck() {
+        return "Application is running successfully!";
+    }
+}
