a902f9eb468129ef3511d763936aafd5141b72a1
Osama Farag <ofarag115@gmail.com>
2026-03-21 09:32:42 -0400

    created the profileService class along with 2 helper functions

---
+++ src/main/java/com/osama_farag/money_manager/service/ProfileService.java
@@ -0,0 +1,51 @@
+package com.osama_farag.money_manager.service;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.osama_farag.money_manager.dto.ProfileDTO;
+import com.osama_farag.money_manager.entity.ProfileEntity;
+import com.osama_farag.money_manager.repository.ProfileRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService{
+
+    private final ProfileRepository profileRepository;
+
+    public ProfileDTO registerProfile(ProfileDTO profileDTO){
+
+        ProfileEntity newProfile = toEntity(profileDTO); // Convert DTO to Entity (you would typically save this entity to the database here)
+        newProfile.setActivationToken(UUID.randomUUID().toString());
+        newProfile = profileRepository.save(newProfile); // Save the new profile to the database
+
+        return toDTO(newProfile);
+    }
+
+    public ProfileEntity toEntity(ProfileDTO profileDTO){
+        return ProfileEntity.builder()
+                .id(profileDTO.getId())
+                .fullName(profileDTO.getFullName())
+                .email(profileDTO.getEmail())
+                .password(profileDTO.getPassword())
+                .profileImageUrl(profileDTO.getProfileImageUrl())
+                .createdAt(profileDTO.getCreatedAt())
+                .updatedAt(profileDTO.getUpdatedAt())
+                .build();
+    }
+
+    public ProfileDTO toDTO(ProfileEntity profileEntity){
+        return ProfileDTO.builder()
+                .id(profileEntity.getId())
+                .fullName(profileEntity.getFullName())
+                .email(profileEntity.getEmail())
+                //.password(profileEntity.getPassword())
+                .profileImageUrl(profileEntity.getProfileImageUrl())
+                .createdAt(profileEntity.getCreatedAt())
+                .updatedAt(profileEntity.getUpdatedAt())
+                .build();
+    }
+}
