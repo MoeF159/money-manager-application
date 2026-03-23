28aba046ce42a574568342a20de12d685468c510
Osama Farag <ofarag115@gmail.com>
2026-03-23 13:01:11 -0400

    Created AuthDTO class

---
+++ src/main/java/com/osama_farag/money_manager/dto/AuthDTO.java
@@ -0,0 +1,40 @@
+package com.osama_farag.money_manager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthDTO {
+    private String email;
+    private String password;
+    private String token;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
