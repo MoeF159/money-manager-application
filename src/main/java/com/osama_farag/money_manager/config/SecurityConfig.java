9cdf2357b1dce9430d4ef0fff34e75196d83d3e8
Osama Farag <ofarag115@gmail.com>
2026-03-23 11:25:37 -0400

    Created Security Configuration file to manage spring security on endpoints

---
+++ src/main/java/com/osama_farag/money_manager/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.osama_farag.money_manager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+   
+    // Security configuration will be implemented here in the future
+   @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        httpSecurity
+            .cors(Customizer.withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/status", "/health", "/register", "/activate").permitAll()
+                .anyRequest().authenticated()
+            );
+        return httpSecurity.build();
+    }
+}
