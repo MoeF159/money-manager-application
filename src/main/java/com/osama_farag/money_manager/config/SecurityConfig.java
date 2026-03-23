9f2897c2557b9ce2344eccb1a4bff26fb9c46a2f
Osama Farag <ofarag115@gmail.com>
2026-03-23 11:33:39 -0400

    added session management to SecurityConfig

--- src/main/java/com/osama_farag/money_manager/config/SecurityConfig.java
+++ src/main/java/com/osama_farag/money_manager/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
@@ -17,13 +18,19 @@ public class SecurityConfig {
     // Security configuration will be implemented here in the future
    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+        return httpSecurity
             .cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
+                // Allow unauthenticated access to these endpoints
                 .requestMatchers("/status", "/health", "/register", "/activate").permitAll()
+                // All other endpoints require authentication
                 .anyRequest().authenticated()
-            );
-        return httpSecurity.build();
-    }
+            )
+            // Set session management to stateless since we're using JWTs for authentication
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .build();
+        }
 }
