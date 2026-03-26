97175bbd7f1022bae439604c184e98c4f15b70e9
Osama Farag <ofarag115@gmail.com>
2026-03-26 16:57:46 -0400

    Built CategoryController skeleton

---
+++ src/main/java/com/osama_farag/money_manager/controller/CategoryController.java
@@ -0,0 +1,13 @@
+package com.osama_farag.money_manager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+public class CategoryController {
+    
+} 
