68060d11bc018757416f9290f2ede495789147e3
Osama Farag <ofarag115@gmail.com>
2026-03-27 10:59:18 -0400

    Add POST endpoint to create categories
    Introduce a POST /categories endpoint that accepts a CategoryDTO in the request body, delegates creation to CategoryService, and returns the saved CategoryDTO with HTTP 201 Created. Adds required imports and a final CategoryService field (injected via Lombok's @RequiredArgsConstructor).

--- src/main/java/com/osama_farag/money_manager/controller/CategoryController.java
+++ src/main/java/com/osama_farag/money_manager/controller/CategoryController.java
@@ -1,7 +1,14 @@
 package com.osama_farag.money_manager.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.osama_farag.money_manager.dto.CategoryDTO;
+import com.osama_farag.money_manager.service.CategoryService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +17,11 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/categories")
 public class CategoryController {
     
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> saveCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO savedCategory = categoryService.saveCategory(categoryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    }
 } 
