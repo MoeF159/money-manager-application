cac7295dbab6e016ddacd98179d5c574c10971a0
Osama Farag <ofarag115@gmail.com>
2026-03-27 14:54:33 -0400

    Add PUT endpoint to update category
    Expose a new PUT /{categoryId} endpoint in CategoryController to update an existing category. Adds the PutMapping import and an updateCategory method that accepts a path variable and CategoryDTO request body, delegates to categoryService.updateCategory, and returns the updated CategoryDTO in the response.

--- src/main/java/com/osama_farag/money_manager/controller/CategoryController.java
+++ src/main/java/com/osama_farag/money_manager/controller/CategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +40,11 @@ public class CategoryController {
     public ResponseEntity<List<CategoryDTO>> getCategoriesByTypeForCurrentUser(@PathVariable String type){
         List<CategoryDTO> list = categoryService.getCategoriesByTypeForCurrentUser(type);
         return ResponseEntity.ok(list);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long categoryId, @RequestBody CategoryDTO categoryDTO){
+        CategoryDTO updatedCategory = categoryService.updateCategory(categoryId, categoryDTO);
+        return ResponseEntity.ok(updatedCategory);
     }
 } 
