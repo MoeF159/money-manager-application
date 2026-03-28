91d461a1d86887f34083496c569d7c491f06e7bc
Osama Farag <ofarag115@gmail.com>
2026-03-28 16:24:51 -0400

    Add DTO/Entity helpers to Income/Expense services

--- src/main/java/com/osama_farag/money_manager/service/ExpenseService.java
+++ src/main/java/com/osama_farag/money_manager/service/ExpenseService.java
@@ -1,5 +1,50 @@
 package com.osama_farag.money_manager.service;
 
+import org.springframework.stereotype.Service;
+
+import com.osama_farag.money_manager.dto.ExpenseDTO;
+import com.osama_farag.money_manager.entity.CategoryEntity;
+import com.osama_farag.money_manager.entity.ExpenseEntity;
+import com.osama_farag.money_manager.entity.ProfileEntity;
+import com.osama_farag.money_manager.repository.ExpenseRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ExpenseService {
+
+    private final CategoryService categoryService;
+    private final ExpenseRepository expenseRepository;
+
+
+
+
+    //helper methods
+    private ExpenseEntity toEntity(ExpenseDTO dto, ProfileEntity profile, CategoryEntity category) {
+        return ExpenseEntity.builder()
+            .name(dto.getName())
+            .icon(dto.getIcon())
+            .amount(dto.getAmount())
+            .date(dto.getDate())
+            .profile(profile)
+            .category(category)
+            .build();
+    }
+
+
+    private ExpenseDTO toDTO(ExpenseEntity entity) {
+        return ExpenseDTO.builder()
+            .id(entity.getId())
+            .name(entity.getName())
+            .icon(entity.getIcon())
+            .categoryId(entity.getCategory() != null ? entity.getCategory().getId() : null)
+            .categoryName(entity.getCategory() != null ? entity.getCategory().getName(): "N/A")
+            .amount(entity.getAmount())
+            .date(entity.getDate())
+            .createdAt(entity.getCreatedAt())
+            .updatedAt(entity.getUpdatedAt())
+            .build();
+    }
 
 }
--- src/main/java/com/osama_farag/money_manager/service/IncomeService.java
+++ src/main/java/com/osama_farag/money_manager/service/IncomeService.java
@@ -1,5 +1,52 @@
 package com.osama_farag.money_manager.service;
 
+import org.springframework.stereotype.Service;
+
+import com.osama_farag.money_manager.dto.IncomeDTO;
+import com.osama_farag.money_manager.entity.CategoryEntity;
+import com.osama_farag.money_manager.entity.IncomeEntity;
+import com.osama_farag.money_manager.entity.ProfileEntity;
+import com.osama_farag.money_manager.repository.IncomeRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class IncomeService {
+
+
+    private final CategoryService categoryService;
+    private final IncomeRepository incomeRepository;
+
+
+
+
+    //helper methods
+    private IncomeEntity toEntity(IncomeDTO dto, ProfileEntity profile, CategoryEntity category) {
+        return IncomeEntity.builder()
+            .name(dto.getName())
+            .icon(dto.getIcon())
+            .amount(dto.getAmount())
+            .date(dto.getDate())
+            .profile(profile)
+            .category(category)
+            .build();
+    }
+
+
+    private IncomeDTO toDTO(IncomeEntity entity) {
+        return IncomeDTO.builder()
+            .id(entity.getId())
+            .name(entity.getName())
+            .icon(entity.getIcon())
+            .categoryId(entity.getCategory() != null ? entity.getCategory().getId() : null)
+            .categoryName(entity.getCategory() != null ? entity.getCategory().getName(): "N/A")
+            .amount(entity.getAmount())
+            .date(entity.getDate())
+            .createdAt(entity.getCreatedAt())
+            .updatedAt(entity.getUpdatedAt())
+            .build();
+    }
+
 
 }
