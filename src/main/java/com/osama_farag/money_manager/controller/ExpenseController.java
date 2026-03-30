30ad4e214d10e6fdb3561749ffce443287babe0e
Osama Farag <ofarag115@gmail.com>
2026-03-30 12:09:13 -0400

    created endpoint for getting income/expenses

--- src/main/java/com/osama_farag/money_manager/controller/ExpenseController.java
+++ src/main/java/com/osama_farag/money_manager/controller/ExpenseController.java
@@ -1,5 +1,27 @@
 package com.osama_farag.money_manager.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.osama_farag.money_manager.dto.ExpenseDTO;
+import com.osama_farag.money_manager.service.ExpenseService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/expenses")
 public class ExpenseController {
     
+    private final ExpenseService expenseService;
+
+    @PostMapping
+    public ResponseEntity<ExpenseDTO> addExpense(@RequestBody ExpenseDTO dto){
+        ExpenseDTO saved = expenseService.addExpense(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
 }
--- src/main/java/com/osama_farag/money_manager/controller/IncomeController.java
+++ src/main/java/com/osama_farag/money_manager/controller/IncomeController.java
@@ -1,5 +1,27 @@
 package com.osama_farag.money_manager.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.osama_farag.money_manager.dto.IncomeDTO;
+import com.osama_farag.money_manager.service.IncomeService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/incomes")
 public class IncomeController {
-    
+
+    private final IncomeService incomeService;
+
+    @PostMapping
+    public ResponseEntity<IncomeDTO> addIncome(@RequestBody IncomeDTO dto){
+        IncomeDTO saved = incomeService.addIncome(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
 }
